838321118cc14b056880bc745e85e04e7b24194d
bala-amypo <bala@amypo.in>
2025-12-22 04:13:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Campaign.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Campaign {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String campaignName;
+
+    private Date startDate;
+    private Date endDate;
+
+    private BigDecimal budget;
+    private Boolean active = true;
+
+    
+}
---
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Influencer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String socialHandle;
+
+    private String email;
+    private Boolean active = true;
+
+    private Timestamp createdAt;
+
+    @PrePersist
+    void onCreate() {
+        createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    // getters & setters
+}
