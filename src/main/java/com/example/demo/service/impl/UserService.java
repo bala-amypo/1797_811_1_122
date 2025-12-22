8c79ff71206e63003812234bbcf05658064bcbf8
bala-amypo <bala@amypo.in>
2025-12-22 04:42:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserService.java
+++ src/main/java/com/example/demo/service/impl/UserService.java
@@ -1,28 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private final UserRepository repo;
-    private final PasswordEncoder encoder;
+    User registerUser(User user);
 
-    public UserService(UserRepository repo, PasswordEncoder encoder) {
-        this.repo = repo;
-        this.encoder = encoder;
-    }
-
-    public User registerUser(User user) {
-        user.setPassword(encoder.encode(user.getPassword()));
-        return repo.save(user);
-    }
-
-    public User findByEmail(String email) {
-        return repo.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("not found"));
-    }
+    User findByEmail(String email);
 }
