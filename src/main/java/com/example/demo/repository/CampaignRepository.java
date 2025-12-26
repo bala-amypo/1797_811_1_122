d24ce8da7b5183b8df56b63ceef5e2cb45949076
bala-amypo <bala@amypo.in>
2025-12-26 17:07:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/CampaignRepository.java
+++ src/main/java/com/example/demo/repository/CampaignRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.RoiReport;
+import com.example.demo.model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface RoiReportRepository extends JpaRepository<RoiReport, Long> {
-
-    List<RoiReport> findByDiscountCodeInfluencerId(Long influencerId);
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 }
